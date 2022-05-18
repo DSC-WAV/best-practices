840f76c2fe47d1528e6980f99cf0e604f03e7d63
Nicholas Horton <nicholasjhorton@gmail.com>
2022-05-18 19:35:23 -0400

    Adding simplest Shiny app (default template but changed to used ggplot2).

---
+++ shiny/shiny01/app.R
@@ -0,0 +1,49 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        ggplot(faithful,
+          aes(x = waiting)) +
+          geom_histogram(bins = input$bins) + 
+          labs(x = "waiting time to next eruption (in mins)")
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
