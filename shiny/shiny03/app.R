8e58c2b04278f4f6a3fa63256357e6b1f90e7e4f
Nicholas Horton <nicholasjhorton@gmail.com>
2022-05-19 06:19:57 -0400

    Moving files.

--- shiny/shiny03/app.R
+++ shiny/shiny03/app.R
@@ -1,171 +1,91 @@
-#
-# This is a Shiny web application written by two students. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(tidyverse)
 library(shiny)
-library(shinythemes)
-library(leaflet)
-library(mosaic)
+library(shinybusy)
+library(mdsr)
 library(RSQLite)
-library(shinyWidgets)
-library(shinydashboard)
 
-
-# Establish database for storing data
 mydb <- dbConnect(RSQLite::SQLite(), ":memory:")
-init_data <- data.frame(matrix(nrow = 0, ncol = 4))
-colnames(init_data) <- c("latitude", "longitude", "us", "road")
-dbWriteTable(mydb, "location", init_data, overwrite = TRUE)
-dbGetQuery(mydb, "SELECT * FROM location")
+init_data <- tibble(
+  name = c("Nick", "Maura", "Julia", "Ben"), 
+  series = c("The West Wing", "Sports Night", "The Crown", "The Simpsons"),
+  network = c("NBC", "ABC", "Netflix", "Fox")
+)
+dbWriteTable(mydb, "tvseries", init_data, overwrite = TRUE)
 
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(theme = shinytheme("yeti"),
-                # setBackgroundColor(color = "ghostwhite"),
-                useShinydashboard(),
-    # Application title
-    titlePanel("Roadless Group-3: B and K", windowTitle = "Roadless"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            actionButton(inputId = "sample",
-                         label = "Sample Data"),
-            actionButton(inputId = "append",
-                         label = "Add to Data"),
-            checkboxInput(inputId = "USA",
-                           label = "Is this in the US?",
-                          value = F),
-            
-            #### Second Button ####
-            #  to only appear if USA checkbox is checked
-            conditionalPanel(condition = "input.USA == 1", 
-                             checkboxInput("near_road", label = "Is it within 1 mile of a road?", value = F)
-            ),
-            
-            
-            downloadButton("downloadData", "Download"),
-            tableOutput("table")
-            
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            verbatimTextOutput(outputId = "text"),
-            leafletOutput("map"),
-            valueBox(
-                uiOutput("total_count"), "Total Sampled", icon = icon("database"), color = "red"
-            ),
-            valueBox(
-                uiOutput("us_count"), "In the US", icon = icon("flag-usa"), color = "purple"
-            ),
-            valueBox(
-                uiOutput("road_count"), "Near a Road", icon = icon("road")
-            )
-        )
+ui <- fluidPage(
+  titlePanel("TV series database"),
+  # some things take time: this lets users know
+  add_busy_spinner(spin = "fading-circle"),
+  fluidRow(
+    # display dynamic list of networks to select
+    column(4, uiOutput("networkcontrols")),
+    column(4, 
+           textInput(
+             inputId = "download",
+             label = "Download widget goes here:", 
+             value = ""
+           )
     )
+  ),
+  fluidRow(
+    column(4, 
+           textInput(
+             inputId = "nameinput",
+             label = "Name to add:", 
+             value = ""
+           )
+    ),
+    column(4, 
+           textInput(
+             inputId = "seriesinput",
+             label = "Series and network inputs goes here:", 
+             value = ""
+           )
+    ),
+    column(4, 
+           textInput(
+             inputId = "actionbutton",
+             label = "Add the button to add the inputs here:", 
+             value = ""
+           )
+    )
+  ),
+  # Create a new row for the table.
+  fluidRow(
+    DT::dataTableOutput("table")
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-    
-    sample_coordinates <- eventReactive(input$sample, {
-        updateCheckboxInput(session = getDefaultReactiveDomain() , "USA", value = FALSE)
-        updateCheckboxInput(session = getDefaultReactiveDomain() , "near_road", value = FALSE)
-        rgeo(1, latlim = c(25,50), lonlim = c(-65, -125)) 
-    }, ignoreNULL = FALSE)
-    
-    
-    observeEvent(input$USA, {
-        if(!input$USA) {
-            updateCheckboxInput(session = getDefaultReactiveDomain() , "near_road", value = FALSE)
-        } 
-        
-    })
-    
-    
-    output$total_count <- renderText({
-        prettyNum(nrow(db_contents()), big.mark=",")
-    })
-    
-    output$us_count <- renderText({
-        prettyNum(nrow(filter(db_contents(), us == 1)), big.mark=",")
-    })
-    
-    output$road_count <- renderText({
-        prettyNum(nrow(filter(db_contents(), road == 1)), big.mark=",")
-    })
-    
-    output$map <- renderLeaflet({
-        leaflet_map(sample_coordinates(), radius = 1, unit = "miles")  %>%
-            setView(-95, 37.5, zoom = 3.4)
-            # addProviderTiles(providers$Stamen.TonerLite,
-            #                  options = providerTileOptions(noWrap = TRUE)
-            # ) 
-            # addMarkers(data = sample_coordinates())
-    })
-    
-    output$text <- renderText({paste0("Location Status:\nLatitude: ", sample_coordinates()$lat, "\n",
-                                     "Longitude: ", sample_coordinates()$lon, "\n",
-                                     "Is it in the US?: ", input$USA, "\n",
-                                     "Is it within 1 mile of a road?: ", input$near_road, "\n")})
-                                     # "Running Tally: There are ", nrow(db_contents()), " location(s) sampled. ",
-                                     # nrow(filter(db_contents(), us == 1)), " are in the", "\n",
-                                     # "United States while ",
-                                     # nrow(filter(db_contents(), road == 1)), 
-                                     # " of those are within 1 mile ", 
-                                     # "of a road.")})
 
-    
-    output$downloadData <- downloadHandler(
-        filename = function() {
-            paste("download.csv")
-        },
-        content = function(file) {
-            write.csv(db_contents(), file, row.names = FALSE)
-        }
+  db_contents <- reactive({  
+    data <- dbGetQuery(mydb, "SELECT * FROM tvseries")
+    return(data)
+  })
+  
+  db_subset <- reactive({  
+    data <- db_contents()
+    req(input$network)  # wait until there's a selection
+    if (input$network != "ALL") {
+      data <- filter(data, network == input$network)
+    }
+    return(data)
+  })
+  
+  output$networkcontrols <- renderUI({
+    availablelevels <-
+      unique(sort(as.character(db_contents()$network)))
+    selectInput(
+      inputId = "network",
+      label = "Network selector:",
+      choices = c("ALL", availablelevels)
     )
+  })
+  
+  output$table <- DT::renderDataTable(DT::datatable(db_subset()))
 
-    observeEvent(input$append, {
-        
-        data <- dbGetQuery(mydb, "SELECT * FROM location")
-        
-        new_data <- tibble(
-            latitude = sample_coordinates()$lat,
-            longitude = sample_coordinates()$lon,
-            us = input$USA,
-            road = input$near_road
-        )
-    
-        if(!(sample_coordinates()$lat %in% data$lat & sample_coordinates()$lon %in% data$lon)) {
-        dbWriteTable(mydb, "location", new_data, append = TRUE)
-        } else {
-            showModal(modalDialog(
-                title = "Oh no!",
-                paste0("This location has already been recorded you silly little goose!"),
-                easyClose = TRUE,
-                footer = NULL
-            ))
-        }
-    }, priority = 1)
-
-
-    db_contents <- eventReactive(input$append, {
-        cat("button =", input$sample, "\n")
-        data <- dbGetQuery(mydb, "SELECT * FROM location")
-        return(data)
-    }, ignoreNULL = FALSE, ignoreInit = FALSE)
-    
-    output$table <- renderTable({
-        db_contents()
-    })
 }
 
-
-# Run the application 
 shinyApp(ui = ui, server = server)
